f6ec52de4c29d6865d3666540ef6631398fb47c9
ludwhe <_@ludwhe.dev>
2024-01-16 15:15:35 +0100

    Initial commit

---
+++ src/Avro.Generator/SchemaSourceGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Avro.Generator
+{
+    [Generator]
+    public class SchemaSourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // nothing to do here
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var schemas = context.AdditionalFiles
+                .Where(x => Path.GetExtension(x.Path).Equals(".avsc", StringComparison.OrdinalIgnoreCase))
+                .Select(x =>
+                {
+                    var text = x.GetText();
+                    return text != null ? Schema.Parse(text.ToString()) : null;
+                }).Where(x => x != null);
+
+            var codeGen = new CodeGen();
+            foreach (var schema in schemas)
+            {
+                codeGen.AddSchema(schema);
+            }
+
+            codeGen.GenerateCode();
+            var sources = codeGen.GetTypes();
+            foreach (var source in sources)
+            {
+                context.AddSource(source.Key, SourceText.From(source.Value, Encoding.UTF8));
+            }
+        }
+    }
+}
